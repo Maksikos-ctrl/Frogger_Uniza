c063be961b76895419038abef4e08fc174a4bc42
Erik Janošík <janosik3@fri.uniza.sk>
2024-11-29 12:56:40 +0100

    commit

---
+++ Frogger_Uniza-copy/Trieda.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Trieda here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Trieda {
+    /**
+     * Constructor for objects of class Trieda
+     */
+    public Trieda() {
+        // initialise instance variables
+    }
+}

252c39f6f3223937ab045e11d46a745c22780eeb
remen <matusremen295@gmail.com>
2024-11-29 12:35:15 +0100

    commit

---
+++ Frogger_Uniza-copy/HraciaPlocha.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class HraciaPlocha here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HraciaPlocha {
+    /**
+     * Constructor for objects of class HraciaPlocha
+     */
+    public HraciaPlocha() {
+        // initialise instance variables
+    }
+}

d1a8c96d352ebac97076b217355ab5a22d2d47dd
chernikov <chernikov@stud.uniza.sk>
2024-12-19 21:30:11 +0100

    vymazal som nepotrebni subory

--- Frogger_Uniza-copy/src/entities/Zaba.java
+++ Frogger_Uniza-copy/src/entities/Zaba.java
@@ -17,74 +17,75 @@ public class Zaba {
     private int x, y; // Pozícia žaby
     private static final int SIZE = 30; // Veľkosť žaby
     private BufferedImage frogImage; // Obrázok žaby
-
-    /**
-     * Konštruktor inicializuje žabu.
-     */
-   public Zaba() {
-        x = 400;
-        y = 550;
-
-        try {
-            frogImage = ImageIO.read(getClass().getResource("/resources/frog.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
+    private Object i;
+    
+        /**
+         * Konštruktor inicializuje žabu.
+         */
+       public Zaba() {
+            x = 400;
+            y = 550;
+    
+            try {
+                frogImage = ImageIO.read(getClass().getResource("/resources/frog.png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-    }
-
-    /**
-     * Aktualizuje polohu hráča.
-    */
-    public void update() {
-        // Logika pohybu bude pridaná neskôr
-    }
-
-    /**
-     * Nakreslí žabu na obrazovku.
-    * 
-    * @param g Grafický objekt na kreslenie.
-    */
-    public void draw(Graphics g) {
-        if (frogImage != null) {
-            g.drawImage(frogImage, x, y, SIZE, SIZE, null);
-        } else {
-            g.setColor(Color.GREEN);
-            g.fillRect(x, y, SIZE, SIZE);
+    
+        /**
+         * Aktualizuje polohu hráča.
+        */
+        public void update() {
+            // Logika pohybu bude pridaná neskôr
         }
-    }
-
-    /**
-     * Posunie žabu v danom smere.
-    * 
-    * @param dx Posun na osi X.
-    * @param dy Posun na osi Y.
-    */
-    public void move(int dx, int dy) {
-        x += dx;
-        y += dy;
-    }
-
-    public Rectangle getBounds() {
-        return new Rectangle(x, y, SIZE, SIZE);
-    }
-
-    /**
-     * Kontroluje, či sa žaba zrazila s prekážkou.
-    * 
-    * @param obstacle Prekážka na kontrolu kolízie.
-    * @return true, ak sa objekty prekrývajú, inak false.
-    */
-    public boolean intersects(Drevo obstacle) {
-        return getBounds().intersects(obstacle.getBounds());
-    }
-
-    public void getY() {
-        return this.y;
-
-    }
-
-    public void getI() {
-        return this.i;
+    
+        /**
+         * Nakreslí žabu na obrazovku.
+        * 
+        * @param g Grafický objekt na kreslenie.
+        */
+        public void draw(Graphics g) {
+            if (frogImage != null) {
+                g.drawImage(frogImage, x, y, SIZE, SIZE, null);
+            } else {
+                g.setColor(Color.GREEN);
+                g.fillRect(x, y, SIZE, SIZE);
+            }
+        }
+    
+        /**
+         * Posunie žabu v danom smere.
+        * 
+        * @param dx Posun na osi X.
+        * @param dy Posun na osi Y.
+        */
+        public void move(int dx, int dy) {
+            x += dx;
+            y += dy;
+        }
+    
+        public Rectangle getBounds() {
+            return new Rectangle(x, y, SIZE, SIZE);
+        }
+    
+        /**
+         * Kontroluje, či sa žaba zrazila s prekážkou.
+        * 
+        * @param obstacle Prekážka na kontrolu kolízie.
+        * @return true, ak sa objekty prekrývajú, inak false.
+        */
+        public boolean intersects(Drevo obstacle) {
+            return getBounds().intersects(obstacle.getBounds());
+        }
+    
+        public int getY() {
+            return this.y;
+    
+        }
+    
+        public void getI() {
+            return;
 
     }
 }
